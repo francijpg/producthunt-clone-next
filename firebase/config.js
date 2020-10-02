badfbc589688d5359b8b92e2992c1f27c0dd7f7c
francijpg <hellofranciscopg@gmail.com>
2020-10-02 09:44:45 -0300

    add environment variable handling

--- firebase/config.js
+++ firebase/config.js
@@ -1,4 +1,12 @@
 const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DB,
+  projectId: process.env.REACT_APP_PID,
+  storageBucket: process.env.REACT_APP_SB,
+  messagingSenderId: process.env.REACT_APP_SID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MID,
 };
 
 export default firebaseConfig;
